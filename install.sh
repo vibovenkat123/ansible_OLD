1e945c1670f38cc3ef78b2414c7da3fdc3f36036
vibovenkat123 <vibovenkat@gmail.com>
2023-03-01 16:57:42 -0600

    feat: add news apps

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 ansible-galaxy collection install ansible.posix
 ansible-galaxy collection install community.general
-
+defaults write com.apple.dock autohide-delay -float 0; defaults write com.apple.dock autohide-time-modifier -float 0.3;killall Dock
+sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.DiskArbitration.diskarbitrationd.plist DADisableEjectNotification -bool YES && sudo pkill diskarbitrationd
