b9071069e1fce862ab7d6a6478a97c8d6fe688f0
Vaibhav Venkat <vibovenkat@gmail.com>
2023-05-07 14:03:33 -0500

    feat: rectange setup

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ ansible-galaxy collection install community.general
 defaults write com.apple.dock autohide-delay -float 0; defaults write com.apple.dock autohide-time-modifier -float 0.3;killall Dock
 sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.DiskArbitration.diskarbitrationd.plist DADisableEjectNotification -bool YES && sudo pkill diskarbitrationd
 env CGO_ENABLED=0 go install -ldflags="-s -w" github.com/gokcehan/lf@latest
+defaults write com.knollsoft.Rectangle screenEdgeGapTop -int 45
